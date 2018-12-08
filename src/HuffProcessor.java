2216cb5464aec9d8ef296adbdf787b22cfba8e1d
Rachel Hua <rachel.hua@duke.edu>
2018-12-07 23:09:55 -0500

    hello

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -94,8 +94,30 @@ public class HuffProcessor {
 		}
 	}
 	
-	private void readCompressedBits(HuffNode root, BitInputStream in, BitInputStream out) {
-		
+	private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+		int bits;
+		HuffNode current = root;
+		while(true) {
+			bits = in.readBits(1);
+			if(bits == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else {
+				if(bits == 0) current = current.myLeft;
+				else {
+					if(current.myLeft == null && current.myRight == null) {
+						if(current.myValue == PSEUDO_EOF) {
+							break;
+						}
+						else {
+							out.writeBits(BITS_PER_WORD, current.myValue);
+							current = root;
+						}
+					}
+				}
+			}
+			
+		}
 		
 	}
 }
